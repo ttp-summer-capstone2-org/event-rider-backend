0d057df977f4d51efb0042d11d73482fbc46f8eb
Fausto Vazquez <faustovazquez.005@gmail.com>
2026-07-29 13:09:22 -0400

    feat: Model Association

--- models/index.js
+++ models/index.js
@@ -5,7 +5,17 @@ import Rides from "./rideModel.js";
 import Users from "./userModel.js";
 import Events from "./eventModel.js";
 
+Users.hasMany(Tickets, { foreignKey: 'user_id' });
+Tickets.belongsTo(Users, { foreignKey: 'user_id' });
 
+Events.hasMany(Tickets, { foreignKey: 'event_id' });
+Tickets.belongsTo(Events, { foreignKey: 'event_id' });
+
+Tickets.hasMany(Rides, { foreignKey: 'ticket_id' });
+Rides.belongsTo(Tickets, { foreignKey: 'ticket_id' });
+
+Drivers.hasMany(Rides, { foreignKey: 'driver_id' });
+Rides.belongsTo(Drivers, { foreignKey: 'driver_id' });
 
 
 export { dbConnection, Drivers, Tickets, Users, Rides, Events };
