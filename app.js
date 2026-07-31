7435e5b45d8654ba886833a88fe7d7db1d0a1433
Fausto Vazquez <faustovazquez.005@gmail.com>
2026-07-31 10:26:37 -0400

    feat: auth with sign up, login and me routes

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ import driverRoutes from './routes/driverRoutes.js'
 import rideRoutes from './routes/rideRoutes.js'
 import ticketRoutes from './routes/ticketRoutes.js'
 import authRoutes from './routes/authRoutes.js'
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
